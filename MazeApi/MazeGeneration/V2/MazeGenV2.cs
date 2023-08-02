8a0d813197442b910fa22bf6d29d18678c2680cc
Jose Vincoski <josevincoski.gmail.com>
2023-08-01 21:25:59 -0300

    Removed unecessary function from MazeGenV2

--- MazeApi/MazeGeneration/V2/MazeGenV2.cs
+++ MazeApi/MazeGeneration/V2/MazeGenV2.cs
@@ -58,34 +58,5 @@ namespace MazeGeneratorLib.V2
         {
             return rnd.Next(1, value);
         }
-
-
-        public void GenerateMaze(ref Maze maze, bool a = false)
-        {
-            for (int row = 1; row < maze.Height; row += 2)
-            {
-                for (int column = 1; column < maze.Width; column += 2)
-                {
-                    var tilesAroundInfo = new TilesAroundInfo(maze, row, column);
-
-                    if (tilesAroundInfo.MovableWalls.Count > 1)
-                    {
-                        var numberOfWallsToCarve = GetRandomIntLowerThan(tilesAroundInfo.MovableWalls.Count);
-
-                        for (int i = 0; i < numberOfWallsToCarve; i++)
-                        {
-                            var wallToChange = tilesAroundInfo.MovableWalls.GetRandomElement<TileInfo>(rnd).Position;
-                            maze.Tiles[wallToChange.Y, wallToChange.X] = (int)TileType.Path;
-                        }
-
-                        if (tilesAroundInfo.MovableWalls.Count == 1 && rnd.Next(11) < 7)
-                        {
-                            var wallToChange = tilesAroundInfo.MovableWalls.First().Position;
-                            maze.Tiles[wallToChange.Y, wallToChange.X] = (int)TileType.SolidWall;
-                        }
-                    }
-                }
-            }
-        }
     }
 }
